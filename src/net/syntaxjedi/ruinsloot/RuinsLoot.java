7d1d7bdafb639e7392e50db5aff49e21390fc31a
syntaxjedi <syntaxjedi@gmail.com>
2017-11-12 12:19:55 -0600

    +fixed dupe bug +added name support
    Fixed the duplication bug and added name support. Chests now try to fill when loading chunks and only after set time. New bugs:
    - chests only face one way
    - if facing the wrong way when placed, chests lose their name

--- src/net/syntaxjedi/ruinsloot/RuinsLoot.java
+++ src/net/syntaxjedi/ruinsloot/RuinsLoot.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -14,34 +13,42 @@ import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
-import org.bukkit.block.Container;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
-public class RuinsLoot extends JavaPlugin {
+public class RuinsLoot extends JavaPlugin implements Listener {
 	
 	private static final Logger log = Logger.getLogger("Minecraft");
 	public static Inventory inv;
+	public static Block bl;
 	public int time;
 	public Long timel;
+	public boolean waitRef = false;
 	@Override
 	public void onEnable(){
 		//loadConfiguration();
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
+		this.getServer().getPluginManager().registerEvents(this, this);
 		
 		time = getConfig().getInt("time.ticks");
 		timel = (long)time;
@@ -49,7 +56,9 @@ public class RuinsLoot extends JavaPlugin {
 	        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
 	            @Override
 	            public void run() {
+	                waitRef = true;
 	                findChest();
+	                log.info("waited");
 	            }
 	        }, 0L, timel);
 	}
@@ -88,56 +97,34 @@ public class RuinsLoot extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command command, String label, String args[]){
 		
 		Player p = (Player) sender;
-		if(sender != p){
-			log.info("This command can only be run by players currently");
-			return false;
-		}
 		if(command.getLabel().equalsIgnoreCase("lootchest") && sender == p){
 			
 
 			if(args.length > 0 && args.length < 2){
-				if(args[0].equalsIgnoreCase("common")){
+				if(args[0].equalsIgnoreCase("common") || args[0].equalsIgnoreCase("uncommon") || args[0].equalsIgnoreCase("legendary")){
 					
 					int emptySlot = p.getInventory().firstEmpty();
-					p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, "Common LootChest"));
-					p.sendMessage(ChatColor.GOLD + "Gave " + ChatColor.WHITE + "Common LootChest " + ChatColor.GOLD + "to " + p.getDisplayName());
+					String cap = args[0].substring(0, 1).toUpperCase() + args[0].substring(1).toLowerCase();
+					String low = args[0].toLowerCase();
+					p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, cap + " LootChest", low));
+					p.sendMessage(ChatColor.GOLD + "Gave " + ChatColor.WHITE + cap + " LootChest " + ChatColor.GOLD + "to " + p.getDisplayName());
 					return true;
-					
-				}
-				
-				if(args[0].equalsIgnoreCase("uncommon")){
-					
-					int emptySlot = p.getInventory().firstEmpty();
-					p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, "Uncommon LootChest"));
-					p.sendMessage(ChatColor.GOLD + "Gave " + ChatColor.BLUE + "Common LootChest " + ChatColor.GOLD + "to " + p.getDisplayName());
-					return true;
-					
-				}
-				
-				if(args[0].equalsIgnoreCase("legendary")){
-					
-					int emptySlot = p.getInventory().firstEmpty();
-					p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, "Legendary LootChest"));
-					p.sendMessage(ChatColor.GOLD + "Gave " + ChatColor.LIGHT_PURPLE + "Common LootChest " + ChatColor.GOLD + "to " + p.getDisplayName());
-					return true;
-					
-				}
-				
-				if(args[0].equalsIgnoreCase("fill")){
-					
-					findChest();
+				}else if(args[0].equalsIgnoreCase("fill")){
 					p.sendMessage(ChatColor.GOLD + "Filling Chests");
+					waitRef = true;
+					findChest();
 					return true;
-					
 				}
-				
 				else{
 					p.sendMessage(ChatColor.RED + "You must append the proper arguments.");
 					return false;
 				}
-			}else if(args.length > 1 && args.length < 3){
+			}else if(args.length > 1 && args.length < 3 && args[0].equalsIgnoreCase("common") || args[0].equalsIgnoreCase("uncommon") || args[0].equalsIgnoreCase("legendary")){
+				
 				int emptySlot = p.getInventory().firstEmpty();
-				p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, args[1]));
+				String low = args[0].toLowerCase();
+				p.getInventory().setItem(emptySlot, createItem(Material.CHEST, 1, args[1], low));
+				p.sendMessage(ChatColor.GOLD + "Gave " + ChatColor.WHITE + low +" LootChest " + ChatColor.GOLD + "to " + p.getDisplayName());
 				return true;
 			}
 			if(args.length > 3){
@@ -156,38 +143,70 @@ public class RuinsLoot extends JavaPlugin {
 		}
 	}
 	
+	@EventHandler
+	public void onBlockPlaceEvent(BlockPlaceEvent e){
+		Player p = (Player)e.getPlayer();
+		World world = p.getWorld();
+		ItemStack is = p.getInventory().getItemInMainHand();
+		ItemMeta meta = is.getItemMeta();
+		if(is.getType() == Material.CHEST && meta.getLore().contains("common") || meta.getLore().contains("uncommon") || meta.getLore().contains("legendary")){
+			//e.setCancelled(true);
+			Block loc = e.getBlockAgainst();
+			Block lc = loc.getLocation().add(0, +1, 0 ).getBlock();
+			lc.setType(Material.CHEST);
+			lc.setMetadata(meta.getLore().get(0), new FixedMetadataValue(this, "lootChest"));
+		}
+	}
+	
+	@EventHandler
+	public void onChunkLoadEvent(ChunkLoadEvent e){
+		if(!e.isNewChunk()){
+			findChest();
+		}
+	}
+	
 	public void findChest(){
-		log.info("Refilling Chests");
 		World world = Bukkit.getWorld("world");
-		for(Chunk c : world.getLoadedChunks()){
-			for(BlockState b : c.getTileEntities()){
-				if(b instanceof Chest){
-					inv = ((Chest) b).getBlockInventory();
-					if(inv.getName().equalsIgnoreCase("common lootchest")){
-						inv.clear();
-						ItemStack[] stacks = getLoot("common");
-						for(int i = 0; i < stacks.length; i++){
-							inv.setItem(i, stacks[i]);
+		if(waitRef){
+			for(Chunk c : world.getLoadedChunks()){
+				for(BlockState b : c.getTileEntities()){
+					if(b instanceof Chest){
+						inv = ((Chest) b).getBlockInventory();
+						//bl = (Block) b;
+						
+						if(b.hasMetadata("common")){
+							inv.clear();
+							ItemStack[] stacks = getLoot("common");
+							for(int i = 0; i < stacks.length; i++){
+								inv.setItem(i, stacks[i]);
+							}
+						}
+						else if(b.hasMetadata("uncommon")){
+							log.info("UnCommon!");
+							inv.clear();
+							ItemStack[] stacks = getLoot("uncommon");
+							for(int i = 0; i < stacks.length; i++){
+								inv.setItem(i, stacks[i]);
+							}
+						}
+						else if(b.hasMetadata("legendary")){
+							inv.clear();
+							ItemStack[] stacks = getLoot("legendary");
+							for(int i = 0; i < stacks.length; i++){
+								inv.setItem(i, stacks[i]);
+							}
 						}
 					}
-					else if(inv.getName().equalsIgnoreCase("uncommon lootchest")){
-						inv.clear();
-						ItemStack[] stacks = getLoot("uncommon");
-						for(int i = 0; i < stacks.length; i++){
-							inv.setItem(i, stacks[i]);
-						}
-					}
-					else if(inv.getName().equalsIgnoreCase("legendary lootchest")){
-						inv.clear();
-						ItemStack[] stacks = getLoot("legendary");
-						for(int i = 0; i < stacks.length; i++){
-							inv.setItem(i, stacks[i]);
-						}
+					else{
+						continue;
 					}
 				}
 			}
+		}else if(!waitRef){
+			return;
 		}
-		log.info("done");
+		waitRef = false;
+		log.info("Filled Chests");
 	}
 	
 	public ItemStack[] getLoot(String chest){
@@ -196,10 +215,15 @@ public class RuinsLoot extends JavaPlugin {
 		return stacks;
 	}
 	
-	public static ItemStack createItem(Material material, int amount, String name){
+	public static ItemStack createItem(Material material, int amount, String name, String type){
 		ItemStack item = new ItemStack(material, amount);
 		ItemMeta meta = item.getItemMeta();
 		if(name != null) meta.setDisplayName(name);
+		if(type != null){
+			List<String> chestType = new ArrayList<String>();
+			chestType.add(type);
+			meta.setLore(chestType);
+		}
 		item.setItemMeta(meta);
 		return item;
 	}
